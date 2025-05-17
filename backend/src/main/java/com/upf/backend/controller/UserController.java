63557ccae0e212b9dc563e371ceaa55fbebc0f06
rioVito <vitoriobeti@gmail.com>
2025-05-16 21:19:57 -0300

    Criado Crud basico no backend, inciado personalizacao do frontend com Bootstrap. Frontend ja mapeado para salvar as coisas no banco.

---
+++ backend/src/main/java/com/upf/backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.upf.backend.controller;
+
+import com.upf.backend.model.User;
+import com.upf.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+
+    }
+
+
+}
---
+++ backend/src/main/java/com/upf/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.upf.backend.repository;
+
+import com.upf.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
