14a61f854f5e75fb6c8605eb4de0e02d7d2e1188
rioVito <vitoriobeti@gmail.com>
2025-05-18 16:23:40 -0300

    pesonalizacao inicial navbar

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
 export default function Navbar() {
-  return (
-    <div>
+    return (
+        <div>
 
-    
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarcontent">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <button className="btn btn-outline-light">Adicionar Usuario</button>
+                </div>
+            </nav>
 
 
-    </div>
-  )
+        </div>
+    )
 }
