4706c8589e554f54d031372ebd0b94673c26b7e5
rioVito <vitoriobeti@gmail.com>
2025-05-18 16:41:30 -0300

    merge de backend e frontend na main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './layout/Navbar.js';
+import Home from './pages/Home.js';
 
 function App() {
   return (
     <div className="App">
-
-
       <Navbar />
+      <Home />
     </div>
   );
 }
