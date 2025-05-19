cc28f428f503e49bd317a6c8c824a31dfb8f99e8
VIto <200763@upf.br>
2025-05-19 20:39:23 -0300

    criacao da classe de categorias

---
+++ backend/src/main/java/com/upf/backend/model/Category.java
@@ -0,0 +1,56 @@
+package com.upf.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Priority color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Priority getColor() {
+        return color;
+    }
+
+    public void setColor(Priority color) {
+        this.color = color;
+    }
+
+    public enum Priority {
+        BAIXA, MEDIA, ALTA
+    }
+}
