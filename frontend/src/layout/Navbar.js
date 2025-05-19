cf5b92466a8303c0b751c13ecb4e23f6b570b583
VIto <200763@upf.br>
2025-05-19 20:28:41 -0300

    alterado alinhamento dos botoes na navbar

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -6,12 +6,15 @@ export default function Navbar() {
         <div>
 
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container-fluid">
+                <div className="m-2">
                     <a className="navbar-brand">TowerPad</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarcontent">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
+                    <button className="btn btnoutline-light" type="button">
+                        <Link className="btn btn-outline-light" to="/">Usuarios</Link>
+                    </button>
                     
                 </div>
             </nav>
